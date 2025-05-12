5ccc41c7a66d1dad2c1114750b0dfc378bc1c73d
zsthimself <shutiaozhou07@gmail.com>
2025-05-12 17:39:00 +0800

    6

--- next.config.ts
+++ next.config.ts
@@ -3,21 +3,11 @@ import type { NextConfig } from "next";
 // 判断是否为生产环境
 const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    unoptimized: true,
-  },
+const nextConfig = {
+  images: { unoptimized: true },
   trailingSlash: true,
-  eslint: {
-    // 在生产构建时忽略ESLint错误
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // 在生产构建时忽略TypeScript错误
-    ignoreBuildErrors: true,
-  },
-  // 只在生产环境中指定资源前缀为自定义域名，开发环境使用默认空字符串
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
   assetPrefix: isProd ? 'https://naridiaai.pro' : '',
 };
 
