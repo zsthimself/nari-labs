a87f1524eaba3e9ba8826ee3369ed2b867cd9934
zsthimself <shutiaozhou07@gmail.com>
2025-05-12 18:49:24 +0800

    9

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
+  output: 'export',
   images: { unoptimized: true },
   trailingSlash: true,
   eslint: { ignoreDuringBuilds: true },
